e1ce41332215d16686eae58bd4655d214e0440a0
Drotte <158179502+Drotte@users.noreply.github.com>
2024-11-18 18:07:27 +0100

    Update Test hushållskvot.R

--- Test hushållskvot.R
+++ Test hushållskvot.R
@@ -35,10 +35,6 @@ response <- GET(data_url_prog)
 metadata <- content(response, "text", encoding = "UTF-8")
 metadata_json <- fromJSON(metadata)
 
-# Visa metadata
-print(metadata_json)
-view(metadata_json)
-
 # Skapa JSON-begäran för POST-anrop
 query_prog <- list(
   query = list(
@@ -60,19 +56,18 @@ data_prog <- as.data.frame(data$data)
 
 data_prog <- data_prog %>%
   mutate(key = gsub('c\\(|\\)|"', '', key)) %>%
-  separate(key, into = c("Region", "Ålder", "Tid"), sep = ",")
+  separate(key, into = c("Region", "Alder", "Tid"), sep = ",")
 
 
-# Lägg till den specifika dataset-endpointen för nuvarande och tidigare befolkning
+
+
+# Hämtar historisk demografisk data
 data_url_hist <- paste0(base_url, "/BE/BE0101/BE0101A/BefolkningNy")
 
-# OBS!!! Kommer säkert behöva justeras. Hoppas att det inte är några problem att skriva över tidigare värden
-#Hämta metadata för att förstå datasetets struktur
 response <- GET(data_url_hist)
 metadata <- content(response, "text", encoding = "UTF-8")
 metadata_json <- fromJSON(metadata)
 
-# Skapa JSON-begäran för POST-anrop
 query_hist <- list(
   query = list(
     list(code = "Tid", selection = list(filter = "item", values = as.character(ar_hist))),
@@ -82,37 +77,32 @@ query_hist <- list(
   response = list(format = "json")
 )
 
-# Skicka POST-anrop och hämta data
 response <- POST(data_url_hist, body = toJSON(query_hist, auto_unbox = TRUE), encode = "json")
 data_json_hist <- content(response, "text", encoding = "UTF-8")
 data <- fromJSON(data_json_hist)
 
 
-# Omvandla till data.frame
 data_hist <- as.data.frame(data$data)
 
 data_hist <- data_hist %>%
   mutate(key = gsub('c\\(|\\)|"', '', key)) %>%
   mutate(values = gsub('c\\(|\\)|"', '', values)) %>%
-  separate(key, into = c("Region", "Ålder", "Tid"), sep = ",") %>%
+  separate(key, into = c("Region", "Alder", "Tid"), sep = ",") %>%
   separate(values, into = c("values", "bef_forandring", sep = ","))
 
 data_hist <- data_hist %>%
-  select(Region, Ålder, Tid, values)
+  select(Region, Alder, Tid, values)
 
 
 
+# Hämtar bostads data
+data_url_bost <- paste0(base_url, "/BO/BO0104/BO0104D/BO0104T04")
 
-# Lägg till den specifika dataset-endpointen för nuvarande och tidigare bostadsbestånd
-data_url_bost <- paste0(base_url, "XXXXXXX")
 
-# OBS!!! Kommer säkert behöva justeras. Hoppas att det inte är några problem att skriva över tidigare värden
-#Hämta metadata för att förstå datasetets struktur
 response <- GET(data_url_bost)
 metadata <- content(response, "text", encoding = "UTF-8")
 metadata_json <- fromJSON(metadata)
 
-# Skapa JSON-begäran för POST-anrop
 query_bost <- list(
   query = list(
     list(code = "Tid", selection = list(filter = "item", values = as.character(ar_hist))),
@@ -121,48 +111,81 @@ query_bost <- list(
   response = list(format = "json")
 )
 
-# Skicka POST-anrop och hämta data
-response <- POST(data_url_bost, body = toJSON(query_bost), encode = "json")
-data_json <- content(response, "text", encoding = "UTF-8")
-data <- fromJSON(data_json)
+response <- POST(data_url_bost, body = toJSON(query_bost, auto_unbox = TRUE), encode = "json")
+data_json_bost <- content(response, "text", encoding = "UTF-8")
+data <- fromJSON(data_json_bost)
 
-# Omvandla till data.frame
 data_bost <- as.data.frame(data$data)
-print(data_bost)
+
+data_bost <- data_bost %>%
+  mutate(key = gsub('c\\(|\\)|"', '', key)) %>%
+  mutate(values = gsub('c\\(|\\)|"', '', values)) %>%
+  separate(key, into = c("Region", "Tid"), sep = ",") %>%
+  rename(bostader = values)
 
 
 # ----------------------------------
 
 #Importera kvot data
-kvot <- read_xlsx()
+kvot <- read_xlsx("kvot.xlsx")
 
-#Importera och formatera demografisk data
-test_df <- read_xlsx("test.xlsx")
+kvot <- kvot %>%
+  mutate(Alder = as.numeric(Alder)) 
 
-test_df <- test_df %>%
-  mutate(test_df, man_kvot = man * kvot,
-         kvinna_kvot = kvinna * kvot,
-         pro_hushall = man_kvot + kvinna_kvot)
+# Skapar en gemensam df för all demografisk data.
+dem_data <- rbind(data_hist, data_prog) %>%
+  mutate(values = as.numeric(values)) %>%
+  mutate(Alder = as.numeric(Alder)) %>%
+  mutate(Tid = as.numeric(Tid))
+
+dem_data <- dem_data %>%
+  left_join(kvot, by = "Alder")
 
 
-Total_hushall_per_ar <- test_df %>%
-  group_by(ar) %>%
-  summarize(hushall_per_ar = sum(pro_hushall, na.rm = TRUE))
+dem_data <- dem_data %>%
+  mutate(dem_data, forva_hushall = values * kvot)
 
+dem_data <- dem_data %>%
+  group_by(Region, Tid) %>%
+  summarise(forv_hushall_per_ar = sum(forva_hushall, na.rm = TRUE))
 
-#Importera data bostäder
-test_bostader <- read_excel("test_bostader.xlsx")
+data_bost <- data_bost %>%
+  mutate(Tid = as.numeric(Tid))
 
-# väver ihop två df grupperat på år
-bostader_mot_husall <- left_join(Total_hushall_per_ar, test_bostader, by = "ar")
+total <- dem_data %>%
+  left_join(data_bost, by = c("Region", "Tid")) %>%
+  mutate(bostader = as.numeric(bostader))
 
-# beräknar över / underskott
-bostader_mot_husall <- bostader_mot_husall %>%
-  mutate(bostader_mot_husall, ovar_underskott = hushall_per_ar - bostader)
+#Beräkna över- underskott av bostäder
+bost_prog <- total %>%
+  mutate(underskott = forv_hushall_per_ar - bostader) # %>%
+#  mutate(underskott = ifelse(underskott < 0, 0, underskott)) %>% frågan om denna ska få vara kvar. kan vara bra att veta inför andra beräkningar
+#  mutate(ackumulerad_underskott = cumsum(underskott)) 
 
-bostader_mot_husall <- bostader_mot_husall %>%
-  mutate(ackumulerad_underskott = cumsum(ovar_underskott))
+forandring <- bost_prog %>%
+  filter(Tid %in% c(stat_ar, slut_ar)) %>%
+  select(Region, forv_hushall_per_ar)
+
+forandring <- bost_prog %>%
+  filter(Tid %in% c(stat_ar, slut_ar)) %>%
+  arrange(Region, Tid) %>% 
+  group_by(Region) %>%
+  summarise(skillnad = diff(forv_hushall_per_ar)) %>%
+  mutate(vaxer_regionen = skillnad > 0)
+
+bost_prog <- bost_prog %>%
+  left_join(forandring, by = "Region")
   
+if (bost_prog) {
+  prognos_vaxer <- prognosDF # OBS!!!! Fixa denna är inte klar. Vi behöver implementera prognos data.
+  mutate(bostader = ifelse(Tid == stat_ar + 1, hushall_per_ar * 1,01))
+}
+  
+#Checkar om antalet hushåll förväntas växa. Om SANT läggs en buffert på antalet bostäder som behövs
+
+
+
+
 #Beräknar den procentuella ökningen bland hushållen
 tidigaste_ar_talet <- min(bostader_mot_husall$ar, na.rm = TRUE) #Här tar vi reda på det minsta värdet
 
